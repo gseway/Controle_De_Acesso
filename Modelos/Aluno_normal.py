49ade7ed3bcfbe756656b2ce2d8d473cbf497890
Douglas Oliveira Azambuja <douglas.azambuja@ufsc.br>
2019-09-30 21:35:27 -0300

    Revert "agora vai"
    This reverts commit 5955bec0c4344b8de63a3502e4633f1b1560e97e.

--- Modelos/Aluno_normal.py
+++ Modelos/Aluno_normal.py
@@ -1,3 +1,9 @@
+from Pesssoa import Pessoa
+import Enum_tipoAluno
 
-Class Aluno_normal:
+Class Aluno_normal(Pessoa):
 
+    __init__(self, nome: str, matricula: int, tipo_aluno: tipo_aluno):
+        super()__init__(nome, matricula, tipo_pessoa)
+
+        
