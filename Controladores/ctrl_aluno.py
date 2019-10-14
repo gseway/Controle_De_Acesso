c6c6d4ff47cd40d569877c6d06eb7f11e03e9581
Gabriel <sewaybrickergabriel@gmail.com>
2019-10-14 11:20:03 -0300

    t

--- Controladores/ctrl_aluno.py
+++ Controladores/ctrl_aluno.py
@@ -1,18 +1,50 @@
+import sys
 from Modelos import Aluno_normal
-from Controladores import Ctrl_principal
-from Telas import Tela_presidente
-from Enum import enum_tipoAluno
+from Telas import Tela_aluno
 
 
 class Ctrl_aluno:
     def __init__(self, Aluno: Aluno_normal):
-        self.Aluno_normal = Aluno
+        self.__alunos = []
 
-    def CriaAluno(self, nome, matricula, enum_tipoAluno):
-        self.Aluno_normal.add_Alunos(nome, matricula, enum_tipoAluno)
+    def inclui_aluno(self, novo_aluno: Aluno_normal):
+        for aluno in self.__alunos:
+            if aluno.get_matricula() == novo_aluno.get_matricula():
+                # se existir um aluno com a mesma matricula, retorna None e para o loop
+                print("Já existe um aluno com essa matricula")
+                return
+        self.__alunos.append(novo_aluno)
 
-    def removeAluno(self, index):
-        self.Aluno_normal.removeAluno(index)
+    def lista_alunos(self):
+        index = 0
+        if not self.__alunos:
+            print("Não há nenhum aluno cadastrado.")
+        for __alunos in self.__alunos:
+            index += 1
+            print(
+                f"{index}-Nome: {__alunos.get_nome()}    Matricula: {__alunos.get_matricula()}")
 
-    def lista(self):
-        pass
+    def atualiza_aluno(self, aluno: Aluno_normal):
+        for index in self.__alunos:
+            if index.matricula == aluno.matricula:
+                index.nome = aluno.nome
+                index.matricula = aluno.matricula
+
+    def exclui_aluno(self, matricula: str):
+        self.__alunos.pop(self.__alunos.index(self.aluno_por_matricula(matricula)))
+
+    def aluno_por_matricula(self, matricula):
+        for aln in self.__alunos:
+            if matricula == aln.get_matricula():
+                return aln
+        print("Nenhum aluno encontrado!")
+        return
+
+    def finalizar(self):
+        sys.exit(0)
+
+    def getalunos(self):
+        return self.__alunos
+
+    def tela_aluno(self):
+        Tela_aluno.tela_aluno.menu_aluno()
--- Controladores/ctrl_principal.py
+++ Controladores/ctrl_principal.py
@@ -1,11 +1,18 @@
 from Telas import Tela_principal
 from Controladores import ctrl_alunoPresidente
-from Controladores import Ctrl_aluno
+from Controladores import ctrl_aluno
 
-class Ctrl_Principal:
 
-    def login_presidente(self):
-        ctrl_alunoPresidente.Tela_presidente.Tela_presidente.menu_principal()
-    
-    def login_aluno(self):
-        Ctrl_aluno
+class ctrl_Principal:
+
+    def login_presidente(self, usuario, senha):
+
+        if usuario in ctrl_alunoPresidente.Ctrl_alunoPresidente.getPresidentes(self):
+            ctrl_alunoPresidente.Tela_presidente.Tela_presidente.menu_principal()
+
+    def login_aluno(self, usuario, senha):
+        if usuario in ctrl_aluno.Aluno_normal.Aluno_normal.getAlunos() and ctrl_aluno.Aluno_normal.Aluno_normal.getAlunos(
+                usuario):
+            Tela_principal.Tela_Principal.login_efetuado()
+        else:
+            Tela_principal.Tela_Principal.falha_login()
